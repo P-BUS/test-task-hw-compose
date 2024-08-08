217f4bb4d3ea1e424fdc2b7b3ddbd44c8821d3ce
P-BUS <pavlo.buslowski@gmail.com>
2024-08-08 19:39:33 +0200

    pic added

--- app/src/main/java/com/books/app/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/books/app/presentation/screens/splash/SplashScreen.kt
@@ -1,12 +1,16 @@
 package com.books.app.presentation.screens.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.books.app.R
 import com.books.app.presentation.navigation.Home
 import kotlinx.coroutines.delay
 
@@ -25,6 +29,18 @@ fun SplashScreen(
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.back),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.heart_back),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
         Text(text = "this is a Splash screen")
     }
 }
