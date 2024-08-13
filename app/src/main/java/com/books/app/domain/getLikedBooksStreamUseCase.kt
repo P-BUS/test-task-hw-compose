f82164b4d23ac5af18fffe06c73fde3db2c22fac
P-BUS <pavlo.buslowski@gmail.com>
2024-08-13 19:40:53 +0200

    reactive approach saved

--- app/src/main/java/com/books/app/domain/getLikedBooksStreamUseCase.kt
+++ app/src/main/java/com/books/app/domain/getLikedBooksStreamUseCase.kt
@@ -3,18 +3,16 @@ package com.books.app.domain
 import com.books.app.data.config.ConfigServiceAPI
 import com.books.app.data.model.Book
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetLikedBooksStreamUseCase @Inject constructor(
     private val configServiceApi: ConfigServiceAPI
 ) {
-    operator fun invoke(): Flow<List<Book>> = flow {
-        configServiceApi.config.collect { config ->
-            val likedList: List<Book> = config.youWillLikeSection.mapNotNull { bookId ->
+    operator fun invoke(): Flow<List<Book>> =
+        configServiceApi.config.map { config ->
+            config.youWillLikeSection.mapNotNull { bookId ->
                 config.books.find { it.id == bookId }
             }
-            emit(likedList)
         }
-    }
 }
